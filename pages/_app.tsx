dbc191f5a2553340135442f5915d8472a255a703
Mikolaj Dutkiewicz <mikolajdutkiewicz@gmail.com>
2023-01-30 12:15:11 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import ScrollObserver from "../utils/scroll-observer";
+import { SizeObserver } from "../utils/size-observer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SizeObserver>
+      <ScrollObserver>
+        <Component {...pageProps} />
+      </ScrollObserver>
+    </SizeObserver>
+  );
 }
